ef60e00c47568d88587d02c2707175e8d302c2f1
Rohan <rohnpatil95@gmail.com>
2021-06-19 19:53:31 +0530

    minor changes

--- Models/ShopItem.cs
+++ Models/ShopItem.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
-
+using System.ComponentModel;
 
 namespace AsyncShopBridge.Models
 {
@@ -13,12 +13,19 @@ namespace AsyncShopBridge.Models
     {
         [Key]
         public int Id { get; set; }
+        [DisplayName("Name")]
         [Required(ErrorMessage = "Please enter product Name")]
         public string productName { get; set; }
+
+        [DisplayName("Price")]
         [Required(ErrorMessage = "Please enter product Price")]
         public Nullable<decimal> productPrice { get; set; }
+
+        [DisplayName("Description")]
         [Required(ErrorMessage = "Please enter product Description")]
         public string productDescription { get; set; }
+
+        [DisplayName("Quantity")]
         [Required(ErrorMessage = "Please enter product quantity")]
         [Range(1, 1000, ErrorMessage = "Enter quantity between 1 to 1000")]
         public int productQuantity { get; set; }
